3696e182657d34f541a3bc49619a7cb5ddd4c110
GabrielPiscke <gabrielmateuspiscke@gmail.com>
2025-08-14 14:26:45 -0300

    UpdInicialC/PastaCss

--- mobile-app/app/(tabs)/index.tsx
+++ mobile-app/app/(tabs)/index.tsx
@@ -1,39 +1,24 @@
-import { StyleSheet, Button } from "react-native";
+import { TouchableOpacity, View, Text } from "react-native";
 import { useRouter } from "expo-router";
-import { Text, View } from "@/components/Themed";
+import { FontAwesome5 } from "@expo/vector-icons"; // ← Aqui muda
+import styles from "../../assets/Styles/index";
 
 export default function InitialScreen() {
   const router = useRouter();
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Bem-vindo ao meu projeto!</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <Button
-        title="Iniciar o Controle"
-        onPress={() => router.push("/controlScreen")}
-      />
+      <View>
+        <FontAwesome5 size={28} name="car-battery" /> {/* ← Agora funciona */}
+      </View>
+
+      <Text style={styles.title}>Bem-vindo!</Text>
+
+      <Text style={styles.subTitle}>Pronto para assumir o controle?</Text>
+
+      <TouchableOpacity style={styles.btnInit}>
+        <Text>Iniciar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
---
+++ mobile-app/assets/Styles/index.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    title: {
+        padding: 2,
+    },
+    subTitle: {
+
+    },
+    btnInit: {
+        
+    }
+})
+
+export default styles;
